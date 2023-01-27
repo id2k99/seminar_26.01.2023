7c4188cf7bc22c807b55a26087628cad503e4ebf
sergey <sergey@solaev.ru>
2023-01-27 19:37:38 +0400

    solved exersice_1

---
+++ exersice_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+} 
+else 
+{
+    Console.WriteLine($"Число {a} не является квадратом числа {b}");
+} 

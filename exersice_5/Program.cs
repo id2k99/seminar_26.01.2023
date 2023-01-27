12a508f2310d7b1f9836a9e683de7528b5a2c245
sergey <sergey@solaev.ru>
2023-01-27 20:34:53 +0400

    solved  exersice_5

---
+++ exersice_5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine(" Введите число");
+int number = int.Parse(Console.ReadLine());
+int i = -1 * number;
+while (i <= number)
+{
+    Console.Write($" {i} ");
+    i ++;
+}

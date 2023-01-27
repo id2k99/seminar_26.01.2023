64f662ba1eb5d36dc5a4f40eb944c4b54c1a2042
sergey <sergey@solaev.ru>
2023-01-27 21:47:10 +0400

    solved 2 with lenght chek  exersice_7

--- Exersice_7/Program.cs
+++ Exersice_7/Program.cs
@@ -21,6 +21,14 @@ Console.WriteLine($" третья цифра: {otvet}"); */
 
 Console.Clear();
 Console.WriteLine(" Введите 3-х значное число: ");
+
 string number = Console.ReadLine();
 string otvet = Convert.ToString(number[number.Length -1]);
+
+if (number.Length <3 || number.Length > 3 )
+{
+    Console.WriteLine(" Число не трёхзначное");
+    return;
+}
+
 Console.WriteLine($" третья цифра: {otvet}");

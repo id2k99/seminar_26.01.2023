4e303658cf0cbb8141610bef59c99dfc5d8ff58e
sergey <sergey@solaev.ru>
2023-01-27 19:24:03 +0400

    solvedd square number example

--- square_number/Program.cs
+++ square_number/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число ");
+
+int number = int.Parse(Console.ReadLine());
+int sqrt = number * number;
+Console.WriteLine($"Квадрат числы {number} равен {sqrt}");
+

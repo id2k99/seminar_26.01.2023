30825dafe67846fe5991fd89f88d7ed3685588ce
sergey <sergey@solaev.ru>
2023-01-27 19:49:46 +0400

    solved exersice_3

---
+++ exersice_3/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+Console.Write(" Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+string a = " Понедельник";
+string b = " Вторник";
+string c = " Среда";
+string d = " Четверг";
+string e = " Пятница";
+string f = " Суббота";
+string g = " Воскресенье";
+
+if (number == 1)
+{
+    Console.WriteLine(a);
+}
+
+if (number == 2)
+{
+    Console.WriteLine(b);
+}
+
+if (number == 3)
+{
+    Console.WriteLine(c);
+}
+
+if (number == 4)
+{
+    Console.WriteLine(d);
+}
+
+if (number == 5)
+{
+    Console.WriteLine(e);
+}
+
+if (number == 6)
+{
+    Console.WriteLine(f);
+}
+
+if (number == 7)
+{
+    Console.WriteLine(g);
+}
+
